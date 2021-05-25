ed694225ea4a3f65dd17fcbe241a59a770c52fe1
Perdente <58706003+Perdente@users.noreply.github.com>
2021-05-25 13:40:49 +0600

    Create FCTRL2 - Small factorials.cpp

---
+++ FCTRL2 - Small factorials.cpp
@@ -0,0 +1,42 @@
+//https://www.spoj.com/problems/FCTRL2/
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+void Malena(){
+	int n;cin>>n;
+	vector<int>v(200);
+	v[0]=1;
+	int m=1;
+	for(int i=1;i<=n;++i){
+		int temp=0;
+		for(int index=0;index<m;++index){
+			int x=i*v[index]+temp;
+			v[index]=x%10;
+			temp=x/10;
+		}
+		while(temp>0){
+			v[m++]=temp%10;
+			temp/=10;
+		}
+	}
+	for(int i=m-1;i>=0;--i){
+		cout<<v[i];
+	}cout<<'\n';
+}
+
+signed main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t;
+	t=1;
+	cin>>t;
+	while(t--){
+		Malena();
+	}
+	return 0;
+}
