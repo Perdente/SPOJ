be6ad182caf35a08d0f6c7cdd60d4541db0ddb21
Perdente <58706003+Perdente@users.noreply.github.com>
2021-05-25 20:08:27 +0600

    Create TEST - Life, the Universe, and Everything.cpp

---
+++ TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,12 @@
+//https://www.spoj.com/problems/TEST/
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+ 
+signed main()
+{
+	int t;cin>>t;
+	while(t!=42)cout<<t<<endl,cin>>t;
+	return 0;
+}
+ 
